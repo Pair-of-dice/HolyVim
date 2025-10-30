e1a0cbcf0b9c69c1765fb2bdd96c1ca2892e6c01
Pairofdice <keonfarrugia@gmail.com>
2025-10-30 11:31:52 +0100

    Removed lua.lua that was in the root directory

--- lua.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_user_command("Luarun", function()
-	local filename = vim.api.nvim_buf_get_name(0)
-	vim.cmd("w")
-	print(vim.fn.system({ "lua", filename }))
-end, { desc = "Run the current Lua buffer" })
-
-vim.keymap.set({ "n" }, "<leader>lle", function()
-	vim.cmd("Luarun")
-end, { desc = "Lua Run" })
