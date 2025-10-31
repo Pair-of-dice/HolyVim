e534d604ffe13b331af06c04fbb4e5236cdf3731
ParadiseofMagic <keonfarrugia@gmail.com>
2025-10-31 21:07:27 +0100

    Commented out mado config.

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -11,18 +11,18 @@ return {
 			lua = { "selene" },
 			markdown = { "mado" },
 		}
-		--		require("lint").linters.mado = {
-		--			cmd = "mado",
-		--			stdin = false, -- or false if it doesn't support content input via stdin. In that case the filename is automatically added to the arguments.
-		--			append_fname = true, -- Automatically append the file name to `args` if `stdin = false` (default: true)
-		--			args = { "check" }, -- list of arguments. Can contain functions with zero arguments that will be evaluated once the linter is used.
-		--			stream = nil, -- ('stdout' | 'stderr' | 'both') configure the stream to which the linter outputs the linting result.
-		--			ignore_exitcode = false, -- set this to true if the linter exits with a code != 0 and that's considered normal.
-		--			env = nil, -- custom environment table to use with the external process. Note that this replaces the *entire* environment, it is not additive.
-		--			parser = require("lint.parser").from_errorformat(efm, {
-		--				source = "mado",
-		--				severity = vim.diagnostic.severity.WARN,
-		--			}),
-		--		}
+--		require("lint").linters.mado = {
+--			cmd = "mado",
+--			stdin = true, -- or false if it doesn't support content input via stdin. In that case the filename is automatically added to the arguments.
+--			args = { "check" }, -- list of arguments. Can contain functions with zero arguments that will be evaluated once the linter is used.
+--			stream = nil, -- ('stdout' | 'stderr' | 'both') configure the stream to which the linter outputs the linting result.
+--			ignore_exitcode = false, -- set this to true if the linter exits with a code != 0 and that's considered normal.
+--			env = nil, -- custom environment table to use with the external process. Note that this replaces the *entire* environment, it is not additive.
+--			parser = require("lint.parser").from_errorformat(efm, {
+--				source = "mado",
+--				severity = vim.diagnostic.severity.WARN,
+--			}),
+--		}
+--		I will do Mado later maybe.
 	end,
 }
