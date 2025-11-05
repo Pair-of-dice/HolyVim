5bc1204fad287b6e3227cd311b49b23ca9893602
ParadiseofMagic <keonfarrugia@gmail.com>
2025-11-05 08:58:00 +0100

    Overided some of Evangelion's colours.

--- lua/plugins/evangelion-theme.lua
+++ lua/plugins/evangelion-theme.lua
@@ -5,6 +5,10 @@ return {
 	opts = {
 		overrides = {
 			Normal = { fg = "#E1D6F8", bg = "#00001a" },
+			TelescopePromptNormal = { bg = "#610044" },
+			LineNr = { fg = "#000018", bg = "#610044" },
+			TelescopeNormal = {bg = "#00001a"},
+			Comment = {fg = "#3e0000", bg = "#39274D"},
 		},
 	},
 	init = function()
